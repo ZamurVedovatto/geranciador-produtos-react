3d89e622ef56bbcf549da91ac3d43ca8748816d3
Zamur <zamurvisual@gmail.com>
2019-07-31 14:39:00 -0300

    Load product data from server

--- README.md
+++ README.md
@@ -58,3 +58,40 @@ renderCategory(cat) {
 { categories.map(this.renderCategory) }
 
 ```
+
+### 6. Load product data from server
+```
+loadData(id) {
+  axios.get(`http://localhost:3001/products?category=`+id)
+  .then(res => {
+    this.setState({
+      products: res.data
+    })
+  })
+}
+
+componentDidMount() {
+  const id = this.props.match.params.catId
+  this.loadData(id)
+}
+
+componentWillReceiveProps(newProps) {
+  const id = newProps.match.params.catId;
+  this.loadData(id)
+}
+
+renderProduto(prod) {
+  return (
+    <p className='bg-light p-2' key={prod.id}>{prod.description}</p>
+  )
+}
+
+render () {
+  return (
+    <div>
+      <h4>Category {this.props.match.params.catId}</h4>
+      {this.state.products.map(this.renderProduto)}
+    </div>
+  )
+}
+```
--- src/Category.js
+++ src/Category.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.loadData = this.loadData.bind(this)
+    this.state = {
+      products: []
+    }
+  }
+
+  loadData(id) {
+    axios.get(`http://localhost:3001/products?category=`+id)
+    .then(res => {
+      this.setState({
+        products: res.data
+      })
+    })
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.catId
+    this.loadData(id)
+  }
+
+  componentWillReceiveProps(newProps) {
+    const id = newProps.match.params.catId;
+    this.loadData(id)
+  }
+
+  renderProduto(prod) {
+    return (
+      <p className='bg-light p-2' key={prod.id}>{prod.description}</p>
+    )
+  }
+
   render () {
-    const { catId } = this.props.match.params
     return (
       <div>
-        <h6>Category {catId}</h6>
+        <h4>Category {this.props.match.params.catId}</h4>
+        {this.state.products.map(this.renderProduto)}
       </div>
     )
   }
